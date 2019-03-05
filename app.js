615145e16a7acf1c2814adef7e8d2f53a90042d4
unknown <santiagojosemontero@gmail.com>
2019-03-05 18:10:27 -0300

    middleware tests done

--- app.js
+++ app.js
@@ -4,9 +4,23 @@
 */
 
 const http = require('http');
+const express = require('express');
 
-const { routes } = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use('/',(req, res, next) => {
+  console.log('This always runs');
+  next();
+});
 
-server.listen(3000, ()=> console.log('node-course listening on port 3000'));
+app.use('/add-product',(req, res, next) => {
+  console.log('I´m the NEXT middleware');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/',(req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+  next();
+});
+
+app.listen(3000, ()=> console.log('node-course listening on port 3000'));
