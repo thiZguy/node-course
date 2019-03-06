77435f4b4d9dd259fe2bd9de875f35b8312c8ca3
unknown <santiagojosemontero@gmail.com>
2019-03-06 17:47:15 -0300

    starting dynamic ocntent module

--- app.js
+++ app.js
@@ -3,24 +3,25 @@
  * @url santiagomf.com
 */
 
-const http = require('http');
 const express = require('express');
+const bodyParser =  require('body-parser');
+const path = require('path');
+
+// relative imports
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 
 const app = express();
 
-app.use('/',(req, res, next) => {
-  console.log('This always runs');
-  next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/add-product',(req, res, next) => {
-  console.log('I´m the NEXT middleware');
-  res.send('<h1>The "Add Product" Page</h1>');
-});
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-  next();
-});
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
 
 app.listen(3000, ()=> console.log('node-course listening on port 3000'));
